f5e0ad226bc0b2d25d62fb7e6d8f90477aeb82a1
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2023-10-26 01:50:54 +0530

    use tree image to spawn a forest.

--- scripts/script.js
+++ scripts/script.js
@@ -14,10 +14,11 @@ function counter(e) {
     collect information on trees
     ------------------------------------------------------------  */
 
-/**
- * @type {string} location of tree image
- */ 
+/** @type {string} location of tree image */ 
 const treeImgSrc = 'assets/images/tree-fir-2.png'
+
+/** @type {{width: number, height: number}} dimensions of the tree image */
+const treeImgDim = { width: 123, height: 280 }
 
 /*  ------------------------------------------------------------
     spawn trees into the forest 
@@ -28,23 +29,28 @@ const forest = document.getElementById("forest")
 
 /**
  * @typedef {Object} SettingsObject
- * @property {number} total - total number of trees in the forest
- * @property {number} rows - number of rows to arrange the trees in 
+ * @property {number} total - a counter: counts total number of trees as they keep getting spawned in the forest. (ps. it starts counting at 0.)
  * @property {number} hSpacing - horizontal spacing between trees (in pixels) 
  * @property {number} vSpacing - vertical spacing between trees (in pixels) 
+ * @property {number} hpadding - horizontal padding inside the #forest div
+ * @property {number} vpadding - vertical padding inside the #forest div
  */
 /** @type {SettingsObject} settings for the forest */
 const fSettings = {
-    total : 20,
-    rows : 5,
-    hSpacing : 20,
-    vSpacing : 40
+    total : 0,
+    hSpacing : 40,
+    vSpacing : 40,
+    hpadding : 60,
+    vpadding : 20
 }
 
-/*  spawn all trees.
-    ps. basic template for the process followed: https://stackoverflow.com/a/66481788 
-    */
-for (let i = 0 ; i < fSettings.total ; i++) {
+/*  spawn all trees. */
+
+let rowID = 0
+let treeIDinRow = 0
+let maxTreeIDinRow = treeIDinRow
+
+for (let i = 0 ; true ; i++) {
     // create new div
     const newDiv = document.createElement("img")
     newDiv.setAttribute('class', 'tree')
@@ -52,13 +58,35 @@ for (let i = 0 ; i < fSettings.total ; i++) {
     // add tres-image into newDiv
     newDiv.setAttribute('src', treeImgSrc)
     // position the tree (so that it sits at the correct location within a desired pattern in the forest)
-    newDiv.style.top = forest.offsetTop + 20 + 0.7 * fSettings.vSpacing * (i % fSettings.rows) + 'px'
-    newDiv.style.left = forest.offsetLeft + 20 + (i * fSettings.hSpacing) + 'px'
+    newDiv.style.left = forest.offsetLeft + fSettings.hpadding + (treeIDinRow * fSettings.hSpacing) + ( rowID % 2 === 0 ? (fSettings.hSpacing/4) : (-fSettings.hSpacing/4) ) + 'px'
+    newDiv.style.top = forest.offsetTop + fSettings.vpadding + fSettings.vSpacing * rowID + 'px'
+    // newDiv.style.left += ((rowID%2) ? (-fSettings.hpadding/2) : (fSettings.hpadding/2))
+    // calculate the bottom-right point of the tree
+    /** @type {number} */
+    const x = Number((newDiv.style.left).substring(0,newDiv.style.left.length-2)) + treeImgDim.width
+    /** @type {number} */
+    const y = Number((newDiv.style.top).substring(0, newDiv.style.top.length - 2)) + treeImgDim.height
+    // draw trees on the next line if you exceed #forest's right-most bounds
+    if (x >= forest.offsetLeft + forest.offsetWidth - fSettings.hpadding) {
+        rowID++
+        treeIDinRow = 0
+    } else {
+        treeIDinRow++
+        // update counter that counts the max number of trees per row
+        maxTreeIDinRow = treeIDinRow >= maxTreeIDinRow ? treeIDinRow : maxTreeIDinRow
+    }
+    // stop drawing trees if you exceed #forest's bottom-most bounds
+    if (y >= forest.offsetTop + forest.offsetHeight - fSettings.vpadding)
+        break;
     // set z-index, so that lower-placed trees seem to be in front
-    newDiv.style.zIndex = (i%fSettings.rows).toString()
+    newDiv.style.zIndex = (i).toString()
     // finally, make the div a child of #forest
     forest.appendChild(newDiv)
+    // update the value for total number of trees spawned in the forest
+    fSettings.total = i+1
 }
+
+console.log(fSettings.total + " trees spawned in " + (rowID) + " rows, with " + (maxTreeIDinRow+1) + " or fewer trees per row.") 
 
 /*  ------------------------------------------------------------
     register whenever a tree is clicked on,
