70ecfa2afb9e9667fb295514dd6f4d5a6ccbbfce
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2023-10-31 16:09:09 +0530

    try: write functions instead of in-line code.

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,50 @@
-/* placeholder javascript file */
-
-
 let burnSound = new Audio('/assets/sound/lighting-a-fire.mp3');
 let safeSound = new Audio('/assets/sound/twinkle.mp3');
 let bgBurn = new Audio('/assets/sound/burningAmbience.mp3');
 let bgForest = new Audio('/assets/sound/forestAmbience.mp3');
 
-let volume = 0;
-let buttons = 1;
-let percentage
+// start playing sounds, on loop
+bgForest.play()
+bgForest.loop = true
+bgForest.volume = 0
+bgBurn.play()
+bgBurn.loop = true
+bgBurn.volume = 0
+
+// vairables for counting number of trees 
+function countTotalTrees() {
+    return /* a number */ document.getElementsByTagName("button").length;
+}
+function percentageOfTrees(c) {
+    return parseFloat((document.getElementsByClassName(c).length) / countTotalTrees())
+}
 
 function stateChanger(b) {
+
+    // toggle classes when a tree is affected
     console.log("toggling 'normal' class.")
     b.classList.toggle("normal");
     console.log("toggling 'burning' class.")
     b.classList.toggle("burning")  
     console.log(b.className)
 
+    // depending on the class in a button, its innerHTML changes
     switch(b.className)
     {
         case "normal":
             b.innerText = "normal tree"
-            bgForest.play()
-            percentage = parseFloat((buttons - (document.getElementsByClassName("yellow").length+1))/buttons)
-            bgForest.volume = 1- percentage
             break;
-        
         case "burning":
             b.innerText = "burning tree"
-            percentage = parseFloat((buttons - (document.getElementsByClassName("orange").length+1))/buttons)
-            bgBurn.volume = 1- percentage
             break;
     }
+
+    setVolumes()
+
+}
+
+/* set the columes of the audio */
+function setVolume() { 
+    bgBurn.volume = percentageOfTrees("burning")
+    bgForest.volume = percentageOfTrees("normal")
 }
