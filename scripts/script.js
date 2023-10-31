11cd039a413e337004bdbb2506eba1203e72dba4
avani-samaga <avani@slow.studio>
2023-10-31 15:17:04 +0530

    not working volume
    errors - cannot access percentage befre initialization, failed to set the volume property(is becoming -1)

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ let bgBurn = new Audio('/assets/sound/burningAmbience.mp3');
 let bgForest = new Audio('/assets/sound/forestAmbience.mp3');
 
 let volume = 0;
+let buttons = 1;
 
 function stateChanger(b) {
     console.log("toggling 'yellow' class.")
@@ -18,32 +19,16 @@ function stateChanger(b) {
     switch(b.className)
     {
         case "yellow":
-            console.log("this is case yellow")
+            b.innerText = "normal tree"
             bgForest.play()
+            let percentage = parseFloat((buttons - (document.getElementsByClassName("yellow").length+1))/buttons)
+            bgForest.volume = percentage
             break;
         
         case "orange":
-            console.log("this is case orange")
+            b.innerText = "burning tree"
+            let percentageBurn = parseFloat((buttons - (document.getElementsByClassName("orange").length+1))/buttons)
+            bgBurn.volume = 1- percentage
             break;
     }
 }
-
-
-// if anything is left clicked(burning) the forest bg fades out and switches to burning bg
-function burningBG()
-{
-    //based on the number of burning trees, the audio will get louder
-    let burnStateTrees = document.getElementsByClassName("button").length
-    //calculating volume percentage
-    let percentage = parseFloat((trees - burnStateTrees)/trees)
-
-    //as soon as a tree is burning we want the nice forest background sound to be replaced with the burning one
-    if(percentage > 0)
-        bgForest.pause()
-    bgBurn.play()
-    //setting volume to change and fade in and out within 2 seconds
-    bgBurn.volume = 1 - percentage
-    console.log("volume should be ", bgBurn.volume)
-}
-  
-//fastSeek function skips to a specific time limit within the audio track- not compatible with many browsers however
