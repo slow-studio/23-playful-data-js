adc04b73fc495be40844e0ad232e0c4b9ea32b2a
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2023-10-31 16:15:01 +0530

    tidy.

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
-let burnSound = new Audio('/assets/sound/lighting-a-fire.mp3');
-let safeSound = new Audio('/assets/sound/twinkle.mp3');
-let bgBurn = new Audio('/assets/sound/burningAmbience.mp3');
-let bgForest = new Audio('/assets/sound/forestAmbience.mp3');
+// get audio files
+const src = "/assets/sound/"
+let burnSound = new Audio(src + 'lighting-a-fire.mp3');
+let safeSound = new Audio(src + 'twinkle.mp3');
+let bgBurn = new Audio(src + 'burningAmbience.mp3');
+let bgForest = new Audio(src + 'forestAmbience.mp3');
 
-// start playing sounds, on loop
-bgForest.play()
+// start playing sounds, on loop, but muted.
+window.onload = bgForest.play()
 bgForest.loop = true
 bgForest.volume = 0
 window.onload = bgBurn.play()
