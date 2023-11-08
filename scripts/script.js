d1f1f0c0f98dd5a99ae01824b770edd0b11539d1
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2023-11-08 22:19:24 +0530

    merge branch 'try-improvedAppearance' into climate
    Squashed commit of the following:
    
    commit b2a81e88ede553cb8e07f37c9e089f352d9ced5c
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 22:17:57 2023 +0530
    
        randomise colours.
    
    commit bedb358b69b8c4cfa0be8404362933a02579c49f
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 22:17:30 2023 +0530
    
        helper-functions for working with colours.
    
    commit 2f5ad99b09b41dda51d9cd1ba92918bc4dbfae8b
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 22:16:52 2023 +0530
    
        make helper-function more robust.
    
    commit 0571126f28662b64f70b3fd63188dd675d8370a6
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 20:25:19 2023 +0530
    
        style: specify colours in HSL
    
        to make it easier to edit the colours using javascript.
    
    commit 1902520018e4e4e26ef090edfa534b863eb406ae
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 20:01:27 2023 +0530
    
        remove redundant spec.
    
    commit 8eaed735f6e8258045be5fc886ec8f4f97f94465
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 19:26:54 2023 +0530
    
        tidy.
    
    commit db41493d554c81ea81b9794a9a78ec83ea4b101d
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 19:23:31 2023 +0530
    
        minor edit to tree shape.
    
    commit 3b0eaf6bdc06dd858ae40213e8061287658811a6
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 19:22:49 2023 +0530
    
        remove redundant spec.
    
    commit 1f44b8a31a340fc9f2dc98b135561a8987f9b735
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 19:21:59 2023 +0530
    
        tidy.
    
    commit c9f3dbe43504042932e3d72b51aba6015960202b
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 18:59:10 2023 +0530
    
        tidy.
    
    commit ef46d8c656bca6faae5d8d10620931534d9bb993
    Author: shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
    Date:   Wed Nov 8 17:36:52 2023 +0530
    
        simplify {object} svgtree.src.foliage

--- scripts/script.js
+++ scripts/script.js
@@ -36,6 +36,66 @@ function approx(n, p) {
         return n - randomDeviation
 }
 
+/** 
+ * fetch the value of a property in the stylesheet 
+ * @param {*} element
+ * @param {string} property
+ * @returns {string}
+ * |  
+ * example: getStyleProperty(document.getElementById('container'), 'padding-right')
+ */
+function getStyleProperty(element, property) {
+    if (element == 'root')
+        element = document.documentElement
+    if(property.slice(0,3)=='var') { 
+        property = property.slice(property.indexOf("(")+1,property.indexOf(")"))
+    }
+    return window.getComputedStyle(element).getPropertyValue(property).trim()
+    /* note: trim() was used because: https://stackoverflow.com/questions/41725725/access-css-variable-from-javascript#comment107745427_41725782 */
+}
+/**
+ * fetch the value of a property (from the :root element) in the stylesheet 
+ * @param {string} property 
+ * @returns {string}
+ * |  
+ * example: getStylePropertyFromRoot('--fire')
+ */
+function getStylePropertyFromRoot(property) {
+    return getStyleProperty('root', property)
+}
+
+/**
+ * @param {string} c - HSL colour
+ * @param {number} rhby - randomise H by this much % [range: 0-100]
+ * @param {number} rlby - randomise L by this much % [range: 0-100]
+ * @param {boolean} [blocker=false] - blocks this function from randomising the colour
+ * @returns {string}
+ * |  
+ * examples: 
+ * - randomiseHSLColour('hsl(16, 57%, 50%)', 15, 30)
+ * - randomiseHSLColour('--fire', 15, 30)
+ * - randomiseHSLColour('var(--fire)', 15, 30)
+ */
+function randomiseHSLColour(c, rhby, rlby, blocker) {
+    if(c.slice(0,3)=='var') {
+        c = c.slice(c.indexOf("(")+1,c.indexOf(")"))
+    }
+    if(c.slice(0,2)=='--') {
+        c = getStylePropertyFromRoot(c)
+    }
+    let hsl = c.split(",")
+    if(hsl[0].split("(")[0]!="hsl") {
+        console.log(`supplied colour is not in HSL format. so, randomiseHSLColour() can not randomise colour. so: returning colour {string} as-is: ${c}.`)
+        return c
+    }
+    let h = Number(hsl[0].split("(")[1])
+    h = approx(h, rhby) ; if(h<=0 || h>=360) h=0 ; h = Math.floor(h);
+    let s = Number(hsl[1].split("%")[0].trim())
+    let l = Number(hsl[2].split("%")[0].trim())
+    l = approx(l, rlby) ; if(l<=0) l=0 ; if(l>=100) l=100 ; l = Math.floor(l);
+    return `hsl(${h}, ${s}%, ${l}%)`
+}
+
 /**
  * update an element's style
  * @param {*} e - element
@@ -43,7 +103,8 @@ function approx(n, p) {
  * @param {string|number} v - value
  */
 function updateStyle(e, p, v) {
-    e.style.setProperty(p, v)
+    if (e == 'root') e = document.documentElement
+    return e.style.setProperty(p, v)
 }
 
 /** make fires crackle */
@@ -53,11 +114,11 @@ setInterval(function () {
     let fires = document.getElementsByClassName("fire")
     for (let i = 0; i < fires.length; i++) {
         if (fires[i]) {
-            fires[i].style.fill = 'var(--firedarker)'
+            fires[i].style.fill = randomiseHSLColour('--firedarker', 0, 5),
             // console.log("fire gets darker")
             setTimeout(function () {
                 if (fires[i]) {
-                    fires[i].style.fill = 'var(--fire)'
+                    fires[i].style.fill = randomiseHSLColour('--fire', 0, 5)
                     // console.log("fire gets less dark") 
                 }
             }, approx(fireCrackleTime, 50));
@@ -150,15 +211,15 @@ function updateTree(svgelement, state) {
         }
         svgelement.classList.add(tree[id].state.now)
         const settings = tree[id].stateSettings[state]
-        tree[id].shape.foliage.now = settings.shape.foliage ? settings.shape.foliage : ''
-        tree[id].shape.stump.now = settings.shape.stump ? settings.shape.stump : ''
-        tree[id].shape.fire.now = settings.shape.fire ? settings.shape.fire : ''
-        tree[id].shape.burned.now = settings.shape.burned ? settings.shape.burned : ''
-        tree[id].colour.outline.now = settings.colour.outline ? settings.colour.outline : ''
-        tree[id].colour.foliage.now = settings.colour.foliage ? settings.colour.foliage : ''
-        tree[id].colour.stump.now = settings.colour.stump ? settings.colour.stump : ''
-        tree[id].colour.fire.now = settings.colour.fire ? settings.colour.fire : ''
-        tree[id].colour.burned.now = settings.colour.burned ? settings.colour.burned : ''
+        tree[id].shape.foliage.now = settings.shape.foliage
+        tree[id].shape.stump.now = settings.shape.stump
+        tree[id].shape.fire.now = settings.shape.fire 
+        tree[id].shape.burned.now = settings.shape.burned 
+        tree[id].colour.outline.now = settings.colour.outline 
+        tree[id].colour.foliage.now = settings.colour.foliage 
+        tree[id].colour.stump.now = settings.colour.stump 
+        tree[id].colour.fire.now = settings.colour.fire 
+        tree[id].colour.burned.now = settings.colour.burned 
 
         // console.log("change t# " + id)
         // console.log(tree[id])
@@ -166,10 +227,10 @@ function updateTree(svgelement, state) {
         /* tree changes appearance: */
         // -- 1. it updates its svg shape
         svgelement.innerHTML =
-            (settings.shape.foliage ? tree[id].shape.foliage.now : '')
-            + (settings.shape.stump ? tree[id].shape.stump.now : '')
-            + (settings.shape.fire ? tree[id].shape.fire.now : '')
-            + (settings.shape.burned ? tree[id].shape.burned.now : '')
+            tree[id].shape.foliage.now
+            + tree[id].shape.stump.now
+            + tree[id].shape.fire.now
+            + tree[id].shape.burned.now
         // -- 2. it sets the colour for those svg-shapes
         for (const p of foliages) {
             p.style.stroke = tree[id].colour.outline.now
@@ -437,13 +498,11 @@ startButton.addEventListener('click', function () {
     const svgtree = {
         src: {
             starttag: '<svg width="100%" height="100%" viewBox="0 0 134 382" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve">',
-            foliage: {
-                default: '<path class="foliage" d="M40.695,345.956c-10.575,0.728 -20.695,-7.724 -20.695,-18.244c0,-63.798 12.002,-95.673 21.939,-166.478c1.993,-14.2 14.902,-76.508 28.401,-76.508c13.498,0 19.325,56.249 27.506,126.547c6.551,56.295 16.154,87.73 16.154,116.124c0,12.091 -12.368,12.859 -24.318,14.703c-11.949,1.843 -37.191,3.044 -48.987,3.856Z"/>',
-                sway: {
-                    left: '<path class="foliage" d="M40.695,345.956c-10.575,0.728 -20.695,-7.724 -20.695,-18.244c0,-63.798 12.002,-90.003 21.939,-160.808c1.993,-14.2 6.398,-82.178 19.897,-82.178c13.498,0 27.829,53.415 36.01,123.713c6.551,56.295 16.154,90.564 16.154,118.958c0,12.091 -12.368,12.859 -24.318,14.703c-11.949,1.843 -37.191,3.044 -48.987,3.856Z"/>',
-                    right: '<path class="foliage" d="M40.695,343.122c-10.575,0.728 -20.695,-7.724 -20.695,-18.244c0,-63.798 14.837,-95.673 24.774,-166.478c1.993,-14.2 23.406,-73.674 36.905,-73.674c13.498,0 7.986,59.084 16.167,129.382c6.551,56.295 16.154,84.895 16.154,113.289c0,12.091 -12.368,12.859 -24.318,14.703c-11.949,1.843 -37.191,0.21 -48.987,1.022Z"/>',
-                }
-            },
+            foliage: [
+                /* default */ '<path class="foliage" d="M40.695,345.956c-10.575,0.728 -20.695,-7.724 -20.695,-18.244c0,-63.798 12.002,-95.673 21.939,-166.478c1.993,-14.2 14.902,-76.508 28.401,-76.508c13.498,0 19.325,56.249 27.506,126.547c6.551,56.295 16.154,87.73 16.154,116.124c0,12.091 -12.368,12.859 -24.318,14.703c-11.949,1.843 -37.191,3.044 -48.987,3.856Z"/>',
+                /* sway left */ '<path class="foliage" d="M40.695,345.956c-10.575,0.728 -20.695,-7.724 -20.695,-18.244c0,-63.798 12.002,-90.003 21.939,-160.808c1.993,-14.2 6.398,-82.178 19.897,-82.178c13.498,0 27.829,53.415 36.01,123.713c6.551,56.295 16.154,90.564 16.154,118.958c0,12.091 -12.368,12.859 -24.318,14.703c-11.949,1.843 -37.191,3.044 -48.987,3.856Z"/>',
+                /* sway right */ '<path class="foliage" d="M40.695,343.122c-10.575,0.728 -20.695,-7.724 -20.695,-18.244c0,-63.798 14.837,-95.673 24.774,-166.478c1.993,-14.2 23.406,-73.674 36.905,-73.674c13.498,0 7.986,59.084 16.167,129.382c6.551,56.295 16.154,84.895 16.154,113.289c0,12.091 -12.368,12.859 -24.318,14.703c-11.949,1.843 -37.191,0.21 -48.987,1.022Z"/>'
+            ],
             stump: '<path class="stump" d="M78.599,343.603l-0,30.056l-4.484,5.341l-14.596,0l-3.834,-4.884l-0,-28.913l22.914,-1.6Z"/>',
             fire: '<path class="fire" d="M55.889,74.322c3.005,-5.038 6.083,-11.872 4.708,-18.707c-3.06,-15.32 13.79,-28.09 13.79,-28.09c9.68,-6.27 9.366,-17.66 8.466,-23.27c-0.087,-0.726 1.123,-1.254 1.123,-1.254c-0,-0 0.875,0.402 0.987,0.73c1.854,5.7 3.784,16.352 -0.066,22.964c-1.186,2.037 -3,6.51 -3.71,8.22c-1.4,3.46 -2.52,7.77 -2.95,21l0.067,4.954c1.711,6.895 2.653,12.701 2.653,16.696c-0.035,4.892 -1.498,9.668 -4.21,13.74l-5.43,8.22c-4.73,11.03 -4.03,19.18 2.12,24.52l5.376,1.984c0.339,-0.318 4.245,-2.045 4.924,-2.724c4.16,-4.15 7.34,-8.79 5.87,-15.56c-1.53,-7.1 7.11,-13 10.28,-13.84c0.623,-0.162 1.205,-0.452 1.71,-0.85c2.39,-1.85 8.6,-7.59 6.52,-15.49c0,0 4.5,12.91 -2.44,18.28c-0.447,0.347 -0.887,0.707 -1.32,1.08c-3.533,3.079 -5.649,7.478 -5.85,12.16l-0.15,2.74c0.058,8.914 -8.57,19.88 -8.57,19.88c-1.7,2.53 7.707,7.53 7.937,9.53c0.306,2.553 0.333,5.131 0.08,7.69c-0.191,1.885 -1.083,3.631 -2.5,4.89c0,0 -2.519,4.355 -2.93,6.64l-0.18,4.91c-0.51,1.02 1.49,4.6 4.49,5.33c0,0 3.219,1.587 6.45,0.485c4.875,-4.016 1.671,-10.168 0.873,-17.195c-1.53,-13.54 11.74,-18.64 11.74,-18.64c0,0 5.34,-6.76 5,-12c0.91,3.83 1.39,11.12 -4.52,19.12c-3.491,4.635 -4.441,10.63 -2.744,16.018c1.407,-0.462 3.189,-1.425 3.684,-3.548c1.003,-4.35 3.575,-8.183 7.22,-10.76c1.09,-0.77 3.59,-1.14 5.68,-1.4c0.041,-0.005 0.083,-0.007 0.124,-0.007c0.587,-0 0.809,0.483 0.809,1.07c0,0.284 -0.419,0.556 -0.62,0.757c-3.24,3.08 -7.743,9.35 -6.433,21.35c2.12,19.45 -5.61,14.3 -36.76,45.45c-31.15,31.15 -58.52,-6.67 -58.52,-6.67c0,0 -5.198,-3.565 -10.64,-7.865c-4.837,-2.199 -9.177,-5.549 -12.06,-10.525c-2.154,-3.707 -2.977,-7.499 -2.943,-11.168c0.132,-14.29 11.623,-25.092 11.623,-25.092c0,0 -5.241,16.539 -1.213,26.21c0.031,0.044 0.062,0.091 0.093,0.14c2.18,3.43 5.91,4.37 14.1,6.3c2.239,0.527 10.13,1.253 3.18,-8.37c-2.995,-4.148 -3.584,-12.591 -3,-19.11c0.53,-5.75 4,-12.18 7.31,-16.45c1.501,-1.952 5.061,-3.802 7.981,-5.731c1.58,-2.312 2.174,-5.184 1.609,-7.959l-0.499,-6.531c-0.059,-0.1 -0.117,-0.197 -0.171,-0.289c-3.018,-5.159 -4.454,-11.092 -4.13,-17.06l0,-0.69c0.313,-5.917 2.327,-11.62 5.8,-16.42l5.08,-5.77c1.69,-1.38 3.451,-2.754 5.102,-4.023Z"/>',
             burned: '<path class="burned" d="M60.67,380.299l-6.203,-71.311l8.765,-48.949l-1.22,-11.17l-10.36,-26.59l-8.08,-7.49l-17.79,1.87l18.654,-2.525l-12.664,-14.295l20.595,22.139l9.41,22.697l-4.475,-47.256l12.58,-60.01l-2.882,-10.779l0.142,-9.274l-0.142,-18.858l1.684,21.933l3.158,17.011l-11.853,56.776l22.614,-26.26l4.995,-17.914l-3.238,18.689l-23.304,31.16l5.944,60.361l-5.572,47.798l14.054,-35.733l19.14,-11.5l-1.621,-21.767l3.188,20.26l12.313,-11.843l-11.532,14.279l-19.179,12.356l-15.142,39.016l11.791,67.179l-13.77,0Z"/>',
@@ -480,6 +539,7 @@ startButton.addEventListener('click', function () {
      * @property {boolean} orderly.positionally - spawn trees randomly in front or behind each other (by making sure every tree's zIndex is +1 than the previous tree's zIndex)
      * @property {number} orderly.maxZIndexDeviation - if (orderly.positionally == false ) then maxZIndexDeviation defines how disorderly the trees will be 
      * @property {boolean} orderly.shape - are all trees uniformly shaped?
+     * @property {boolean} orderly.colour - should we introduce some randomness in the colours?
      */
     /** @type {SettingsObject} settings for the forest */
     const forestSettings = {
@@ -496,7 +556,8 @@ startButton.addEventListener('click', function () {
         orderly: {
             positionally: false,
             maxZIndexDeviation: 2,
-            shape: true
+            shape: true,
+            colour: true
         }
     }
 
@@ -537,51 +598,51 @@ startButton.addEventListener('click', function () {
             zindex: i + (forestSettings.orderly.positionally ? 0 : Math.pow(-1, Math.floor(2 * Math.random())) * forestSettings.orderly.maxZIndexDeviation),
             shape: {
                 foliage: {
-                    default: svgtree.src.foliage.default,
-                    previous: svgtree.src.foliage.default,
-                    now: forestSettings.orderly.shape ? svgtree.src.foliage.default : (Math.random() < .5 ? svgtree.src.foliage.sway.left : svgtree.src.foliage.sway.right)
+                    default: svgtree.src.foliage[0],
+                    previous: '',
+                    now: ''
                 },
                 stump: {
                     default: svgtree.src.stump,
-                    previous: svgtree.src.stump,
-                    now: svgtree.src.stump
+                    previous: '',
+                    now: ''
                 },
                 fire: {
-                    default: '',
+                    default: svgtree.src.fire,
                     previous: '',
                     now: ''
                 },
                 burned: {
                     default: svgtree.src.burned,
-                    previous: svgtree.src.burned,
-                    now: svgtree.src.burned
+                    previous: '',
+                    now: ''
                 },
             },
             colour: {
                 outline: {
                     default: 'black',
                     previous: '',
-                    now: 'black'
+                    now: ''
                 },
                 foliage: {
                     default: 'white',
                     previous: '',
-                    now: 'var(--green)'
+                    now: ''
                 },
                 stump: {
                     default: 'white',
                     previous: '',
-                    now: 'var(--wood)'
+                    now: ''
                 },
                 fire: {
                     default: 'white',
                     previous: '',
-                    now: 'var(--fire)'
+                    now: ''
                 },
                 burned: {
                     default: 'black',
                     previous: '',
-                    now: 'black'
+                    now: ''
                 }
             },
             dimensions: {
@@ -607,61 +668,61 @@ startButton.addEventListener('click', function () {
                         // stump: svgtree.src.stump
                     },
                     colour: {
-                        // stump: 'var(--wood)'
+                        stump: randomiseHSLColour('--wood', 0, 5, forestSettings.orderly.colour)
                     }
                 },
                 protected: {
                     isProtected: false,
                     shape: {
-                        foliage: svgtree.src.foliage.default,
+                        foliage: forestSettings.orderly.shape ? svgtree.src.foliage[0] : (Math.random() < .5 ? svgtree.src.foliage[1] : svgtree.src.foliage[2]),
                         stump: svgtree.src.stump,
                         fire: false,
                         burned: false
                     },
                     colour: {
                         outline: 'black',
-                        foliage: 'var(--protected)',
-                        stump: 'var(--wood)'
+                        foliage: randomiseHSLColour('--protected', 3, 10, forestSettings.orderly.colour),
+                        stump: randomiseHSLColour('--wood', 0, 5, forestSettings.orderly.colour)
                     }
                 },
                 normal: {
                     shape: {
-                        foliage: svgtree.src.foliage.default,
+                        foliage: forestSettings.orderly.shape ? svgtree.src.foliage[0] : (Math.random() < .5 ? svgtree.src.foliage[1] : svgtree.src.foliage[2]),
                         stump: svgtree.src.stump,
                         fire: false,
                         burned: false
                     },
                     colour: {
                         outline: 'black',
-                        foliage: 'var(--green)',
-                        stump: 'var(--wood)'
+                        foliage: randomiseHSLColour('--green', 3, 10, forestSettings.orderly.colour),
+                        stump: randomiseHSLColour('--wood', 0, 5, forestSettings.orderly.colour)
                     }
                 },
                 dry: {
                     shape: {
-                        foliage: (Math.random() < .5 ? svgtree.src.foliage.sway.left : svgtree.src.foliage.sway.right),
+                        foliage: (Math.random() < .5 ? svgtree.src.foliage[1] : svgtree.src.foliage[2]),
                         stump: svgtree.src.stump,
                         fire: false,
                         burned: false
                     },
                     colour: {
                         outline: 'black',
-                        foliage: 'var(--autumn)',
-                        stump: 'var(--wood)'
+                        foliage: randomiseHSLColour('--autumn', 10, 20, forestSettings.orderly.colour),
+                        stump: randomiseHSLColour('--wood', 0, 5, forestSettings.orderly.colour)
                     }
                 },
                 burning: {
                     shape: {
-                        foliage: (Math.random() < .5 ? svgtree.src.foliage.sway.left : svgtree.src.foliage.sway.right),
+                        foliage: (Math.random() < .5 ? svgtree.src.foliage[1] : svgtree.src.foliage[2]),
                         stump: svgtree.src.stump,
                         fire: svgtree.src.fire,
                         burned: false
                     },
                     colour: {
                         outline: 'black',
-                        foliage: 'var(--autumn)',
-                        stump: 'var(--wood)',
-                        fire: 'var(--fire)'
+                        foliage: randomiseHSLColour('--autumn', 10, 20, forestSettings.orderly.colour),
+                        stump: randomiseHSLColour('--wood', 0, 5, forestSettings.orderly.colour),
+                        fire: randomiseHSLColour('--fire', 0, 5),
                     }
                 },
                 charred: {
