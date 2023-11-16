17be3604449d7fe2eadf929768ef2db9edbc36da
avani-samaga <avani@slow.studio>
2023-11-16 16:58:41 +0530

    add tree lifecycle array
    added images

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 /* placeholder javascript file */
+
+const treeLifecycle = ["23-playful-data-js/tree frames/01.png",
+                        "23-playful-data-js/tree frames/02.png",
+                        "23-playful-data-js/tree frames/03.png",
+                        "23-playful-data-js/tree frames/04.png",
+                        "23-playful-data-js/tree frames/05.png",
+                        "23-playful-data-js/tree frames/06.png",
+                        "23-playful-data-js/tree frames/07.png",
+                        "23-playful-data-js/tree frames/08.png",
+                        "23-playful-data-js/tree frames/09.png",
+                        "23-playful-data-js/tree frames/10.png",
+                        ]   
