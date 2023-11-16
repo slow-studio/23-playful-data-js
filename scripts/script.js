ce6b0e8b27423c13ea122b191100b0e1eca97f55
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2023-11-16 18:34:35 +0530

    tree cycles automatically.

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,33 @@
 /* placeholder javascript file */
 
-const treeLifecycle = ["tree frames/01.png",
-                        "tree frames/02.png",
-                        "tree frames/03.png",
-                        "tree frames/04.png",
-                        "tree frames/05.png",
-                        "tree frames/06.png",
-                        "tree frames/07.png",
-                        "tree frames/08.png",
-                        "tree frames/09.png",
-                        "tree frames/10.png",
-                        ]   
+const treeLifecycle = [
+    "tree frames/01.png",
+    "tree frames/02.png",
+    "tree frames/03.png",
+    "tree frames/04.png",
+    "tree frames/05.png",
+    "tree frames/06.png",
+    "tree frames/07.png",
+    "tree frames/08.png",
+    "tree frames/09.png",
+    "tree frames/10.png",
+    ]  
+    
+const time = [
+    
+]
 
 //const container = document.getElementById('content');
 let i = 1
-function grow()
+let growtree = function grow()
 {
     if(i>=treeLifecycle.length)
         i = 0
 
-    document.getElementById('image').innerHTML = '<img src="' + treeLifecycle[i] + '" />'
+    /** @type {*} */
+    let img = document.getElementById('tree')
+    img.src = treeLifecycle[i]
     i++
 }
+
+setInterval(growtree,600)
