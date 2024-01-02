9b64658be17f38b0c148d346148cb1bf8062426a
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2024-01-02 19:08:52 +0530

    use modules for cheatcodes.

---
+++ scripts/cheatcodes.js
@@ -0,0 +1,56 @@
+import { setInfo, infoBox, showBox, hideBox } from "./script.js";
+import { updateStyle } from "./helpers.js";
+
+/** cheat code helpers */
+export let pauseForestUpdate = false
+
+/**
+ * cheat codes
+ * @param {KeyboardEvent} e 
+ */
+export function cheatcodes(e) {
+  let key = e.key;
+  console.log(`the ${key}-key was pressed on the keyboard.`)
+  switch(key) {
+    case ' ': 
+        pauseForestUpdate = !pauseForestUpdate
+        // offer visual feedback:
+        if(pauseForestUpdate) updateStyle(document.body,"background-color","var(--body-bg-colour-paused)") 
+        else updateStyle(document.body,"background-color","var(--body-bg-colour-running)") 
+        // write to console:
+        console.log(`pause forestUpdate: ${pauseForestUpdate}`)
+        break;
+    case '1':
+        console.log(`update infoBox content: set introductory info.`)
+        setInfo(infoBox, 1)
+        console.log(`show #infoBox.`)
+        showBox(infoBox)
+        break;
+    case '8':
+        console.log(`update infoBox content: nudging person to tap the screen.`)
+        setInfo(infoBox, 8)
+        console.log(`show #infoBox.`)
+        showBox(infoBox)
+        break;
+    case '2':
+        console.log(`update infoBox content: display goal.`)
+        setInfo(infoBox, 2)
+        console.log(`show #infoBox.`)
+        showBox(infoBox)
+        break;
+    case '0':
+        console.log(`update infoBox content: conclusion.`)
+        setInfo(infoBox, 0)
+        console.log(`show #infoBox.`)
+        showBox(infoBox)
+        break;
+    case 'z': 
+        console.log(`show #infoBox.`)
+        showBox(infoBox)
+        break;
+    case 'x': 
+        console.log(`hide #infoBox.`)
+        hideBox(infoBox)
+        break;
+  }
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+import { pauseForestUpdate, cheatcodes } from "./cheatcodes.js";
 import { updateStyle, interpolateHSLColour, randomiseHSLColour, approx, map } from "./helpers.js";
 import { svgtree } from "./svgtree.js";
 
@@ -13,59 +14,6 @@ import { svgtree } from "./svgtree.js";
  *      - helper tool: https://yoksel.github.io/url-encoder/ or https://svgwiz.com/
  */
 
-/** cheat code helpers */
-let pauseForestUpdate = false
-
-/**
- * cheat codes
- * @param {KeyboardEvent} e 
- */
-function cheatcodes(e) {
-  let key = e.key;
-  console.log(`the ${key}-key was pressed on the keyboard.`)
-  switch(key) {
-    case ' ': 
-        pauseForestUpdate = !pauseForestUpdate
-        // offer visual feedback:
-        if(pauseForestUpdate) updateStyle(document.body,"background-color","var(--body-bg-colour-paused)") 
-        else updateStyle(document.body,"background-color","var(--body-bg-colour-running)") 
-        // write to console:
-        console.log(`pause forestUpdate: ${pauseForestUpdate}`)
-        break;
-    case '1':
-        console.log(`update infoBox content: set introductory info.`)
-        setInfo(infoBox, 1)
-        console.log(`show #infoBox.`)
-        showBox(infoBox)
-        break;
-    case '8':
-        console.log(`update infoBox content: nudging person to tap the screen.`)
-        setInfo(infoBox, 8)
-        console.log(`show #infoBox.`)
-        showBox(infoBox)
-        break;
-    case '2':
-        console.log(`update infoBox content: display goal.`)
-        setInfo(infoBox, 2)
-        console.log(`show #infoBox.`)
-        showBox(infoBox)
-        break;
-    case '0':
-        console.log(`update infoBox content: conclusion.`)
-        setInfo(infoBox, 0)
-        console.log(`show #infoBox.`)
-        showBox(infoBox)
-        break;
-    case 'z': 
-        console.log(`show #infoBox.`)
-        showBox(infoBox)
-        break;
-    case 'x': 
-        console.log(`hide #infoBox.`)
-        hideBox(infoBox)
-        break;
-  }
-}
 document.body.addEventListener('keydown', function(event) {cheatcodes(event)})
 
 let FRAMECOUNT = 0
@@ -654,7 +602,7 @@ function playSound(sound, volume) {
     ------------------------------------------------------------  */
 
 /** @type {HTMLElement} */
-const infoBox = document.getElementById('infoBox')
+export const infoBox = document.getElementById('infoBox')
 
 const infoBoxTransitionDuration = 600
 const showBoxDelayDuration = 600
@@ -667,7 +615,7 @@ setInfo(infoBox, 1)
  * @param {HTMLElement} box
  * @param {number} infotype - 1: intro | 2: display task | 8: instructions to tap | 0: conclusion
  */
-function setInfo(box, infotype) {
+export function setInfo(box, infotype) {
     box.setAttribute('infotype', infotype.toString())
     // first, empty-out the box
     box.innerHTML = ``
@@ -752,7 +700,7 @@ function boxDisplayAttrIs(box) {
 /**
  * @param HTMLElement} box 
  */
-function showBox(box) {
+export function showBox(box) {
     box.setAttribute('display', true) // note: keep this statement outside the setTimeout(), to prevent showBox() from being called multiple times before the delayed actions (below) happen.
     const infotype = Number(box.getAttribute('infotype'))
     setTimeout(function() {
@@ -776,7 +724,7 @@ function showBox(box) {
  * @param {HTMLElement} box 
  * @param {boolean} [seed=true] - seedDryTrees when box closes?
  */
-function hideBox(box, seed) {
+export function hideBox(box, seed) {
     box.setAttribute('display', 'false')
     console.log(`hiding infoBox.`)
     box.style.bottom = `-100vh`
