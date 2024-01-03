84b940551536a558fbd048e74e0eb6343fa701e4
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2024-01-03 15:42:40 +0530

    update code for sticky header

--- script.js
+++ script.js
@@ -91,20 +91,5 @@ function disableScroll() {
   window.addEventListener('keydown', preventDefaultForScrollKeys, false);
 }
 
-window.onscroll = function () { myFunction() };
-var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
 // when the script loads, call the disableScroll() function
 disableScroll();
