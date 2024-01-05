a0a098e4be29163b84e320899151976428e1b582
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2024-01-05 11:11:56 +0530

    tidy: refactor.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 import { pauseForestUpdate, cheatcodes } from "./cheatcodes.js";
+import { infoBox, infoBoxTransitionDuration, setInfo, showBox, boxDisplayAttrIs } from "./infoBox.js";
 import { gainNode } from "./sound.js";
 import { svgtree } from "./svgtree.js";
 
@@ -247,170 +248,6 @@ window.onload = () => {
 	helpers
 	------------------------------------------------------------  */
 
-/** @type {HTMLElement} */
-export const infoBox = document.getElementById('infoBox')
-
-const infoBoxTransitionDuration = 600
-const showBoxDelayDuration = 600
-
-/**
- * @param {HTMLElement} box
- * @param {number} infotype - 1: intro | 2: display task | 8: instructions to tap | 0: conclusion
- */
-export function setInfo(box, infotype) {
-    box.setAttribute('infotype', infotype.toString())
-    // first, empty-out the box
-    box.innerHTML = ``
-    // populate the box
-    switch(infotype) {
-        case 1:
-            // introduction
-            let i1 = addChildTag(box, 'h3')
-            i1.innerHTML = `plant your forest.`
-            let i2 = addChildTag(box, 'p')
-            i2.innerHTML = `tap on the earth to help nurture a tree.`
-            break
-        case 2:
-            // display task
-            let g1 = addChildTag(box, 'h3')
-            g1.innerHTML = `take care of your forest.`
-            let g2 = addChildTag(box, 'p')
-            g2.innerHTML = `tap on a dry or burning tree to save it.`
-            break
-        case 8:
-            // instructions to tap
-            let t1 = addChildTag(box, 'h3')
-            t1.innerHTML = `you can save the forest.`
-            let t2 = addChildTag(box, 'p')
-            t2.innerHTML = `please tap on a dry or burning tree to save it.`
-            break
-        case 0:
-            // conclusion
-            let c1 = addChildTag(box, 'h3')
-            c1.innerHTML = `thank you for playing.`
-            let c2 = addChildTag(box, 'p')
-            c2.innerHTML = `please read about why we made this.`
-            break
-    }
-    // add close-button to dismiss box
-    if(
-        true 
-        && infotype!=0
-    ) {
-        let closeBtn = addChildTag(box, 'button')
-        closeBtn.innerHTML = infotype==1?'<p>go to the forest:</p>':'<p>return to the forest.</p>'
-        closeBtn.setAttribute('id', 'closeInfoBox')
-        closeBtn.addEventListener('click', () => {
-            hideBox(infoBox, true)
-            showcontent(false)
-        })
-    }
-    // add button to reveal essay
-    if(
-        infotype==0 
-        || infotype==1 
-        || gameState.playTime>=1000*60
-    ) {
-        let readBtn = addChildTag(box, 'button')
-        readBtn.innerHTML = '<p>read about this project.</p>'
-        readBtn.setAttribute('id', 'read')
-        readBtn.addEventListener('click', () => showcontent(true))
-    }
-
-    /** 
-     * @param {string} tag  
-     */
-    function addChildTag(parent, tag) {
-        let child = document.createElement(tag)
-        parent.appendChild(child)
-        return child
-    }
-}
-
-/**
- * @returns {boolean} tracks whether the element is displayed or not 
- * @param {HTMLElement} box
- */
-function boxDisplayAttrIs(box) {
-    const attr = box.getAttribute('display')
-    switch(attr) {
-        case "true": return true
-        case "false": return false
-    }
-}
-
-/**
- * @param HTMLElement} box 
- */
-export function showBox(box) {
-    box.setAttribute('display', true) // note: keep this statement outside the setTimeout(), to prevent showBox() from being called multiple times before the delayed actions (below) happen.
-    const infotype = Number(box.getAttribute('infotype'))
-    setTimeout(function() {
-        // sound:
-        if(gameState.userHasBeenActive) {
-            switch(infotype) {
-                case 0:
-                case 1: 
-                case 8: 
-                    // can playSound here
-                    break
-                case 2: 
-                    // can playSound here
-                    break
-            }
-        }
-        // visual:
-        box.style.height = `fit-content`
-        box.style.height = `${box.offsetHeight}px` //`calc(100vh - 2rem)`
-        box.style.bottom = `1rem` //`calc(100vh - 1rem)`
-    }, showBoxDelayDuration)
-}
-
-/**
- * @param {HTMLElement} box 
- * @param {boolean} [seed=true] - seedDryTrees when box closes?
- */
-export function hideBox(box, seed) {
-    box.setAttribute('display', 'false')
-    console.log(`hiding infoBox.`)
-    box.style.bottom = `-100vh`
-    box.style.height = "0"
-    if (seed) {
-        let seeds = 1
-        const infotype = Number(box.getAttribute('infotype'))
-        if (infotype==2) {
-            console.log(`goal-task displayed. will now seed ${seeds} dry tree${seeds==1?'':'s'}.`)
-            seedDryTrees(Math.max(seeds, 1))
-        }
-    } else console.log(`dry-trees will *not* be seeded.`)
-    const infotype = Number(box.getAttribute('infotype'))
-    switch(infotype) {
-        case 1: 
-            startExperience() 
-            gameState.shownInfo1 = true 
-            gameState.shownInfo2 = false
-            console.log(`seen info #1.`) 
-            gameState.print == true 
-            break
-        case 2: 
-            startExperience() 
-            gameState.shownInfo2 = true 
-            console.log(`seen info #2.`) 
-            gameState.print == true 
-            break
-        case 8: 
-            gameState.shownInfo8 = true 
-            console.log(`seen info #8.`) 
-            gameState.print == true 
-            break
-        case 0: 
-            gameState.shownInfo0 = true 
-            console.log(`seen info #0.`) 
-            gameState.print == true 
-            break
-    }
-}
-
 /**
  * @typedef {Object} SettingsObject
  * @property {object} padding - padding inside the #forest div
@@ -429,7 +266,7 @@ export function hideBox(box, seed) {
  */
 
 
-function startExperience() {
+export function startExperience() {
     gameState.startTime = Date.now()
     gameState.playTime = Date.now() - gameState.startTime
     gameState.starthealth = document.getElementsByClassName("normal").length / totalTreesInForest
@@ -443,7 +280,7 @@ function startExperience() {
  * show or hide #content div. (it appears below the #forest div, and has the project writeup in it.)
  * @param {boolean} show 
  */
-function showcontent(show) {
+export function showcontent(show) {
     const contentdiv = document.getElementById('content')
     updateStyle(contentdiv, 'border-top', show ? '.5rem solid black' : 'none')
     updateStyle(contentdiv, 'height', show ? 'fit-content' : '0')
@@ -460,7 +297,7 @@ function showcontent(show) {
  * randomly convert some "normal" trees to their "dry" state
  * @param {number} [n=1] - number of trees to seed
  */
-function seedDryTrees(n) {
+export function seedDryTrees(n) {
 
     console.log(`seedDryTrees(${n}) was called`)
 
@@ -532,7 +369,7 @@ function seedDryTrees(n) {
  * @param {number} [spreadDistance=1]
  * @param {boolean} [spreadUniformly=true]
  */
-function spreadInfection(trees, state, immunity, spreadDistance, spreadUniformly) {
+export function spreadInfection(trees, state, immunity, spreadDistance, spreadUniformly) {
     for (let i = 0; i < trees.length; i++) {
         const id = Number(trees[i].getAttribute('tree-id'))
         const _x = tree[id].positionInForestGrid.x
@@ -615,7 +452,7 @@ function spreadInfection(trees, state, immunity, spreadDistance, spreadUniformly
 /**
  * @param {*} svgelement 
  */
-function updateTree(svgelement) {
+export function updateTree(svgelement) {
 
     // helper variables
     const id = Number(svgelement.getAttribute('tree-id'))
@@ -905,13 +742,13 @@ function updateTree(svgelement) {
 
 // count the number of trees in any particular state
 /** @param {string} state */
-function percentageOfTrees(state) {
+export function percentageOfTrees(state) {
     let trees = document.getElementsByClassName(state)
     return Number(trees.length / totalTreesInForest)
 }
 
 /** calls itself at the end of each animation frame */
-function updateForest() {
+export function updateForest() {
 
     if(Date.now() - gameState.lastUpdatedAt < REFRESH_TIME) {
         // do nothing
@@ -1319,7 +1156,7 @@ function didClickHappenOnTree(e) {
  * @param {number} n - reference number
  * @param {number} [p=20] - deviation %. (eg. for 10% deviation, p = 10). default = 20% deviation.
 */
-function approx(n, p) {
+export function approx(n, p) {
     const maxDeviation = n * (p / 100)
     const randomDeviation = Math.random() * maxDeviation
     if (Math.random() < .5)
@@ -1338,7 +1175,7 @@ function approx(n, p) {
  * @param {number} max2 - upper limit of destination range
  * @param {boolean} [clamp=false] 
  */
-function map(value1, min1, max1, min2, max2, clamp) {
+export function map(value1, min1, max1, min2, max2, clamp) {
     if(min1==max1) {
         console.log(`the source range is invalid. (min and max values in the range must not be equal.) returning unchanged value.`)
         return value1
@@ -1363,7 +1200,7 @@ function map(value1, min1, max1, min2, max2, clamp) {
  * examples: 
  * - randomiseCustomHSLColourProperty('--fire', 15, 30)
  */
-function randomiseCustomHSLColourProperty(c, rhby, rlby, blocker) {
+export function randomiseCustomHSLColourProperty(c, rhby, rlby, blocker) {
     let successflag = false
     let errorcode = 0
     const fallbackcolour = "hsl(0, 0%, 0%)"
@@ -1414,7 +1251,7 @@ function randomiseCustomHSLColourProperty(c, rhby, rlby, blocker) {
  * @param {number} [factorForL=1] 
  * @returns {string}
  */
-function interpolateHSLColour(start, end, totalSteps, currentStep, factorForH,factorForS, factorForL) {
+export function interpolateHSLColour(start, end, totalSteps, currentStep, factorForH,factorForS, factorForL) {
     /** @param {string} c */
     function splitHSL(c) {
         const hsl = c.split(",")
